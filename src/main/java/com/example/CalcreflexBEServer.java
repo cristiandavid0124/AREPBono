24db3049695c1d67e769857bac80fea1ca77a6dc
unknown <cristian.naranjo@labinfo.is.escuelaing.edu.co>
2024-09-19 15:24:36 -0500

    commit servidores

---
+++ src/main/java/com/example/CalcreflexBEServer.java
@@ -0,0 +1,119 @@
+package com.example;
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.MalformedURLException;
+
+public class CalcreflexBEServer {
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(36000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 36000.");
+            System.exit(1);
+        }
+        boolean running = true;
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            boolean isFirstLine = true;
+            String firstLine = "";
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Recibí: " + inputLine);
+                if (isFirstLine) {
+                    firstLine = inputLine;
+                    isFirstLine = false;
+                    break;
+                }
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            URI requrl = getReqURL(firstLine);
+
+            if (requrl.getPath().startsWith("/compreflex")) {
+                outputLine = "HTTP/1.1 200 OK\r\n"
+                        + "Content-Type: application/json\r\n"
+                        + "\r\n"
+                        + "{ \"name\": \"John\", \"age\": 31, \"city\": \"New York\" }";
+            } else {
+                outputLine = htmlClient();
+            }
+
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        serverSocket.close();
+    }
+
+    
+
+    public static String htmlClient() {
+        String htmlCode = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n" +
+                "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "    <head>\n" +
+                "        <title>Form Example</title>\n" +
+                "        <meta charset=\"UTF-8\">\n" +
+                "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    </head>\n" +
+                "    <body>\n" +
+                "        Method not found\n" +
+                "    </body>\n" +
+                "</html>";
+        return htmlCode;
+    }
+
+    public static URI getReqURL(String firstline) throws MalformedURLException, URISyntaxException {
+        String rurl = firstline.split(" ")[1];
+        return new URI(rurl);
+    }
+
+
+    public static double[] bubbleSort(double[] array) {
+        int n = array.length;
+        double temp;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false; 
+    
+            for (int j = 0; j < n - 1 - i; j++) { 
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true; 
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        return array;
+    }
+}
